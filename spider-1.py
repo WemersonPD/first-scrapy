c7cdb9955d5579c9243c2eaccc26e4f97aae14d5
WemersonPD <wpdads10@gmail.com>
2020-11-04 18:28:06 -0300

    Add title, author and texts

--- spider-1.py
+++ spider-1.py
@@ -2,13 +2,24 @@ import scrapy
 
 class MainScpider(scrapy.Spider):
   name = 'main-spider'
-  start_urls = ['http://quotes.toscrape.com/']
+  start_urls = ['https://educacao.uol.com.br/bancoderedacoes/propostas/carnaval-e-apropriacao-cultural.htm']
 
   def parse(self, response):
-    self.log('Eu estou aqui {}'.format(response.url))
-    texts = response.xpath('//span[@class="text"]/text()').extract()
-    for text in texts:
-      yield {
-        'text': text
-      }
-      
+    # self.log('Eu estou aqui {}'.format(response.url))
+    title = response.xpath('//i[@class="col-sm-22 col-md-22 col-lg-22 custom-title"]/text()').get()
+    author = response.xpath('//p[@class="p-author-local"]/text()').get()
+    texts =  response.css('p[style="margin-bottom:11px"]::text').extract()
+
+
+    # texts = response.xpath('//span[@class="text"]/text()').extract()
+    # for text in texts:
+    #   yield {
+    #     'text': text
+    #   }
+    
+    
+    yield {
+      'title': title,
+      'author': author,
+      'texts': texts
+    }
